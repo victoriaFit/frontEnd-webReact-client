dd120c1257ee7efce4d9bd74156b37d014946b5a
Augusto Balsanelli <github.891d8@passfwd.com>
2023-08-22 13:37:26 -0300

    Fixed some bugs and added new content for Home

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,18 +1,20 @@
 import styles from "../../styles/home.module.css";
 import Link from 'next/link';
 
-const Layout = ({ children }) => {
-  return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        <Link href="/" className={styles["header-link"]}>Início</Link>
-        <Link href="/About" className={styles["header-link"]}>Sobre Nós</Link>
-        <Link href="/Services" className={styles["header-link"]}>Serviços</Link>
-        <Link href="/Questions" className={styles["header-link"]}>Dúvidas e FAQ</Link>
-      </header>
-      {children}
-    </div>
-  );
-};
-
+const Layout = ({ children, showHeader = true }) => { 
+    return (
+      <div className={styles.container}>
+        {showHeader && ( 
+          <header className={styles.header}>
+            <Link href="/" className={styles["header-link"]}>Início</Link>
+            <Link href="/About" className={styles["header-link"]}>Sobre Nós</Link>
+            <Link href="/Services" className={styles["header-link"]}>Serviços</Link>
+            <Link href="/Questions" className={styles["header-link"]}>Dúvidas e FAQ</Link>
+          </header>
+        )}
+        {children}
+      </div>
+    );
+  };
+  
 export default Layout;
--- pages/Home/index.js
+++ pages/Home/index.js
@@ -1,14 +1,36 @@
 import Layout from "../../components/Layout";
+import styles from "../../styles/home.module.css";
 
 const Home = () => {
   return (
-    <Layout>
+    <Layout showHeader={false}>
+      <div className={styles.introContainer}>
+        <div className={styles.introText}>
+          <h1>Seja Bem-vindo!</h1> 
+          <p>Explore nossos serviços e descubra como podemos ajudá-lo hoje.</p>
+        </div>
+        <div className={styles.introBlocks}>
+          <div className={styles.upperBlock}>
+            <img src="https://cdn.discordapp.com/attachments/1059425565330911284/1130927105417412608/359996092_1018526755953524_8054183437506109014_n.jpg" alt="Chat" className={styles.icon} />
+            <div>
+              <p>Precisa de uma ajuda para comprar?</p>
+              <small>Fale com um especialista</small>
+            </div>
+          </div>
+          <div className={styles.lowerBlock}>
+            <img src="https://cdn.discordapp.com/attachments/1091506792900595863/1143575792396279939/chat-bubble.png" alt="Chat" className={styles.icon} />
+            <div>
+              <p>Tire suas dúvidas</p>
+              <small>Estamos aqui para ajudar! Clique aqui para começar.</small>
+            </div>
+          </div>
+        </div>
+      </div>
       <div>
         <p>A</p>
       </div>
     </Layout>
   );
 };
-
 
 export default Home;
