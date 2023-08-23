50db71177cf43dfc8126c415aa1562499da992bc
Augusto Balsanelli <github.891d8@passfwd.com>
2023-08-23 14:45:01 -0300

    Updated some items style

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -9,11 +9,9 @@ const Home = () => {
   return (
     <Layout showHeader={false}>
       <div className={styles.introContainer}>
-        <h1 className={styles.introText}>victoria.</h1>
-        <h2 className={styles.introTextSmall}>fit</h2>
         <div className={styles.introText}>
-          <h1>Assistência. Exatamente o que você precisa.</h1>
-          <p>Na Victoria Fit, oferecemos suporte e manutenção especializada para equipamentos de academia. Seja para sua casa ou negócio, estamos aqui para garantir que seus equipamentos estejam sempre em perfeitas condições.</p>
+          <h1><span className={styles.introTextOrange}>Assistência</span>. Exatamente o que você precisa.</h1>
+          <p className={styles.introTextDescription}>Suporte e manutenção especializada para equipamentos de academia. Seja para sua casa ou negócio, estamos aqui para garantir que seus equipamentos estejam sempre em perfeitas condições.</p>
           <button onClick={handleScrollDown}>Descubra Mais</button>
         </div>
       </div>
