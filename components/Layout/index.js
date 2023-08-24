34efdfaabbfc98cb358ec18ed373d6b0ed5ef935
Augusto Balsanelli <github.891d8@passfwd.com>
2023-08-23 21:04:07 -0300

    Finished initial header style

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -11,14 +11,14 @@ const Layout = ({ children, showHeader = true }) => {
   return (
     <div className={isDarkTheme ? styles.darkContainer : styles.container}>
       {showHeader && (
-        <header className={styles.header}>
+        <header className={styles.header} style={{ backgroundColor: isDarkTheme ? 'hsl(207, 8%, 13%)' : 'var(--body-color)' }}>
           <div className={styles.left}>
-            <span className={styles.companyName}>victoria.fit</span>
+          <span className={styles.companyName} style={{ color: isDarkTheme ? '#fff' : 'black' }}>victoria. fit</span>
           </div>
           <div className={styles.right}>
             <img
               src={isDarkTheme
-                ? "https://cdn.discordapp.com/attachments/1091506792900595863/1144045840340619285/moon.png"
+                ? "https://cdn.discordapp.com/attachments/1091506792900595863/1144058096461824010/moon-regular-240.png"
                 : "https://cdn.discordapp.com/attachments/1091506792900595863/1144048381535211550/sun.png"}
               alt="Toggle Theme"
               className={styles.themeButton}
